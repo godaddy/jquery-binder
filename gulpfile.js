702151a56032ad48aa43d8fcb5e02680e19a9c6b
mxryan <mxryan@gmail.com>
2013-09-25 17:43:01 -0700

    Save work in progress... npm run-script build  will now build the dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,21 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var es = require('event-stream');
+var path = require('path');
 
 var cfg = require('./package.json');
 
-gulp.task('minify', function(){
-  gulp.files('./lib/*.js')
-    .pipe(concat({fileName:"jquery.binder." + cfg.version + ".js"}))
+gulp.task('build', function(){
+  console.log("running minify: ", path.resolve('./'));
 
+  //unminified - concat
+  gulp.src('./src/**.js')
+    .pipe(concat({fileName:"jquery.binder." + cfg.version + ".js", separator:'\r\n\r\n'}))
+    .pipe(gulp.dest('./dist'));
 
-  gulp.files('./lib/*.js')
+  //minified
+  gulp.src('./src/*.js')
     .pipe(uglify())
-    .pipe(concat({fileName:"jquery.binder." + cfg.version + ".min.js"}))
+    .pipe(concat({fileName:"jquery.binder." + cfg.version + ".min.js", separator:'\r\n\r\n'}))
+    .pipe(gulp.dest('./dist'));
 });
-
-gulp.task('install',function(){
-  gulp.run('minify');
-});
-
-function prepend(header) {
-  var buffer = [];
-  buffer.push(header)
-}
